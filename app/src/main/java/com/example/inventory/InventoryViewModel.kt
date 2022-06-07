561860ba9a06ad0f8fc4414af7f9071297d2cb4f
Xsufu <aluno.macarrao@gmail.com>
2022-06-07 17:47:16 +0700

    Заполнен класс ViewModel

--- app/src/main/java/com/example/inventory/InventoryViewModel.kt
+++ app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -2,10 +2,51 @@ package com.example.inventory
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.inventory.data.Item
 import com.example.inventory.data.ItemDao
+import kotlinx.coroutines.launch
 
 class InventoryViewModel(private val itemDao: ItemDao): ViewModel(){
+    /**
+     * Обращение к DAO для добавления объекта в БД
+     *
+     * @param item добавляемый объект
+     */
+    private fun insertItem(item: Item) {
+        viewModelScope.launch {
+            itemDao.insert(item)
+        }
+    }
 
+    /**
+     * Получение новой записи о товаре
+     *
+     * @param itemName название товара
+     * @param itemPrice стоимость товара
+     * @param itemCount доступное на складе количество
+     *
+     * @return Экземпляр класса [Item] с заданными параметрами
+     */
+    private fun getNewItemEntry(itemName: String, itemPrice: String, itemCount: String): Item {
+        return Item (
+            itemName = itemName,
+            itemPrice = itemPrice.toDouble(),
+            quantityInStock = itemCount.toInt()
+        )
+    }
+
+    /**
+     * Передача экземпляра класса в функцию добавления в БД
+     *
+     * @param itemName название товара
+     * @param itemPrice стоимость товара
+     * @param itemCount доступное на складе количество
+     */
+    fun addNewItem(itemName: String, itemPrice: String, itemCount: String) {
+        val newItem = getNewItemEntry(itemName, itemPrice, itemCount)
+        insertItem(newItem)
+    }
 }
 
 class InventoryViewModelFactory(private val itemDao: ItemDao): ViewModelProvider.Factory {
